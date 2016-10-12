35eec6df269cb5a081a024cee44758d0a0250417
Justin Lugo <jlug331221@gmail.com>
2016-10-12 10:17:05 -0500

    Basic navbar is complete and mobile friendly. Basic footer design complete.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
     <head>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,15 +13,63 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="{{ asset('css/materialize.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     </head>
 
     <body>
 
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo center">TTU Utilities</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+                <ul id="slide-out" class="side-nav">
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+                <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+
         {{-- Content goes here --}}
-        @yield('content')
+        <main style="height: 80vh;">
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
+
+        {{-- Footer --}}
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col s8">
+                        <img src="{{ asset('storage/images/ttu_logo.svg') }}" alt="TTU Logo" />
+                        <p>
+                            From here, anything is possible. <br>
+                            2500 Broadway Lubbock,Texas 79409 <br>
+                            806.742.2011
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2016 Copyright Text
+                </div>
+            </div>
+        </footer>
 
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/materialize.js') }}"></script>
+
+        <script type="text/javascript">
+
+            $(".button-collapse").sideNav();
+
+        </script>
+
     </body>
 
 </html>
