c580b60c01eb018a77791750f03e08962a4e95ed
Justin Lugo <jlug331221@gmail.com>
2017-02-14 16:03:06 -0600

    Had to change route from 'eraider-sign-in' to '/eraider-sign-in'.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('layouts.welcome-page');
 });
 
-Route::get('eraider-sign-in', function() {
+Route::get('/eraider-sign-in', function() {
     return view('layouts.eraider-authentication');
 });
 
