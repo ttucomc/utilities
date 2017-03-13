2b91382b9eea870c34284d44690e161470805b9a
Justin Lugo <jlug331221@gmail.com>
2017-03-13 13:40:31 -0500

    Add row and column styling to the error pages. Forgot to do that in the last commit.

--- resources/views/errors/unauthorized-access.blade.php
+++ resources/views/errors/unauthorized-access.blade.php
@@ -1,7 +1,12 @@
-<div style="text-align: center;
-            font-size: 72px;
-            color: #B0BEC5;
-            font-weight: 100;
-            font-family: 'Lato', sans-serif;
-            padding-top: 2em;"
-            class="title">You are no Jedi Master!</div>
+<div class="row">
+    <div class="col s12 m8 offset-m2">
+        <div style="text-align: center;
+                    font-size: 72px;
+                    color: #B0BEC5;
+                    font-weight: 100;
+                    font-family: 'Lato', sans-serif;
+                    padding-top: 2em;"
+                    class="title">You are no Jedi Master!
+        </div>
+    </div>
+</div>
--- resources/views/errors/user-not-in-db.blade.php
+++ resources/views/errors/user-not-in-db.blade.php
@@ -1,9 +1,14 @@
-<div style="text-align: center;
-            font-size: 72px;
-            color: #B0BEC5;
-            font-weight: 100;
-            font-family: 'Lato', sans-serif;
-            padding-top: 2em;
-            margin-bottom: 20rem;
-            min-height: 50vh;"
-            class="title">There seems to be a problem here. It looks like you don't exist...<br>Contact an administrator so that we can get you into the system.</div>
+<div class="row">
+    <div class="col s12 m8 offset-m2">
+        <div style="text-align: center;
+                    font-size: 72px;
+                    color: #B0BEC5;
+                    font-weight: 100;
+                    font-family: 'Lato', sans-serif;
+                    padding-top: 2em;
+                    margin-bottom: 20rem;
+                    min-height: 50vh;"
+                    class="title">There seems to be a problem here.<br>It looks like you don't exist...<br>Contact an administrator so that we can get you into the system.
+        </div>
+    </div>
+</div>
